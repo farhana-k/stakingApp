457f8afe3c2aeb0b6327f303931f018dc1ca2878
farhana-k <farhana.kotal@gmail.com>
2023-07-26 09:05:07 +0400

    contract address

--- react-app/src/contract.js
+++ react-app/src/contract.js
@@ -4,8 +4,7 @@ import StakingContract from "./contracts/StakingContract.json"
 
   const web3 = new Web3(ethereum);
 
-  // const ContractAddress = "0xfF4cAa274C92110210b26846C1a4151a317f7906";
-  const ContractAddress = "0xa3236aCA68593A52A2475f3A6C6EbFA8df45CD30";
+  const ContractAddress = "0x2A5a33A2cFbC11C15Cf1697019d58AD8Caf70886";
   const ContractAbi = StakingContract.abi;
   const myContract = new web3.eth.Contract(
     ContractAbi,
